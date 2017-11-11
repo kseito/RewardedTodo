3260a2e65b48725ea4d8ad2459af711cadb18681
k-seito <torinokonoko@gmail.com>
2017-11-11 09:35:10 +0900

    replace Counter.java with Counter.kt

---
+++ app/src/main/java/kztproject/jp/splacounter/model/Counter.kt
@@ -0,0 +1,24 @@
+package kztproject.jp.splacounter.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+import javax.annotation.Generated
+
+@Generated("org.jsonschema2pojo")
+class Counter {
+
+    @SerializedName("id")
+    @Expose
+    var id: Int? = null
+    @SerializedName("count")
+    @Expose
+    var count: Int = 0
+    @SerializedName("created_at")
+    @Expose
+    var createdAt: String? = null
+    @SerializedName("updated_at")
+    @Expose
+    var updatedAt: String? = null
+
+}
