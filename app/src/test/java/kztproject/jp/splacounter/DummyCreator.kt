cf03bc88ff2973ff368dce6e2769a02b778fa96c
k-seito <torinokonoko@gmail.com>
2017-12-17 10:57:13 +0900

    replace DummyCreator.java with DummyCreator.kt

---
+++ app/src/test/java/kztproject/jp/splacounter/DummyCreator.kt
@@ -0,0 +1,24 @@
+package kztproject.jp.splacounter
+
+import kztproject.jp.splacounter.model.Counter
+import kztproject.jp.splacounter.model.User
+import kztproject.jp.splacounter.model.UserResponse
+
+object DummyCreator {
+
+    fun createDummyUserResponse(): UserResponse {
+        val response = UserResponse()
+        val user = User()
+        user.id = 1
+        user.fullName = "test_user"
+        response.user = user
+        return response
+    }
+
+    fun createDummyCounter(): Counter {
+        val counter = Counter()
+        counter.id = 1
+        counter.count = 10
+        return counter
+    }
+}
