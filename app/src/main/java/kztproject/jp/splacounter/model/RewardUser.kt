d864bc831953ad108ef0a38f67697da57a4a3a96
k-seito <torinokonoko@gmail.com>
2018-10-13 11:19:19 +0900

    add new api client

---
+++ app/src/main/java/kztproject/jp/splacounter/model/RewardUser.kt
@@ -0,0 +1,3 @@
+package kztproject.jp.splacounter.model
+
+data class RewardUser(val id: Long, val todoistId: Long, val point: Int)
---
+++ app/src/test/java/kztproject/jp/splacounter/api/HttpMethod.kt
@@ -0,0 +1,7 @@
+package kztproject.jp.splacounter.api
+
+class HttpMethod {
+    companion object {
+        const val POST = "POST"
+    }
+}
