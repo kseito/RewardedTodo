7a16dedc5b0da5de4afb93d6f9b1f88a4a7fee8b
kseito <torinokonoko@gmail.com>
2019-03-30 09:26:10 +0900

    add CompositeDisposable

--- auth/src/main/java/kztproject/jp/splacounter/auth/ui/AuthViewModel.kt
+++ auth/src/main/java/kztproject/jp/splacounter/auth/ui/AuthViewModel.kt
@@ -4,9 +4,11 @@ import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
 import android.support.annotation.StringRes
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kztproject.jp.splacounter.auth.repository.IAuthRepository
 import project.seito.auth.R
+import project.seito.screen_transition.extention.addTo
 import javax.inject.Inject
 
 class AuthViewModel @Inject
@@ -14,6 +16,7 @@ constructor(private val authRepository: IAuthRepository) : ViewModel() {
     private lateinit var callback: Callback
 
     var inputString = ObservableField<String>()
+    private val compositeDisposable = CompositeDisposable()
 
     init {
         inputString.set("")
@@ -35,6 +38,7 @@ constructor(private val authRepository: IAuthRepository) : ViewModel() {
                     .subscribe(
                             { callback.loginSucceeded() }
                     ) { e -> callback.loginFailed(e) }
+                    .addTo(compositeDisposable)
         }
     }
 
@@ -48,8 +52,13 @@ constructor(private val authRepository: IAuthRepository) : ViewModel() {
                     .doOnSubscribe { callback.showProgressDialog() }
                     .doOnTerminate { callback.dismissProgressDialog() }
                     .subscribe({ callback.signUpSucceeded() }, { callback.showError(R.string.error_sign_up) })
-
+                    .addTo(compositeDisposable)
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
     }
 
     interface Callback {
