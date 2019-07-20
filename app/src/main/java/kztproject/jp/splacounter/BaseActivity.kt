12d42da7a2a1d9e6bfbdd1b3a827443aa4436306
kseito <torinokonoko@gmail.com>
2019-07-20 16:02:00 +0900

    add splash screen transition

--- app/src/main/java/kztproject/jp/splacounter/BaseActivity.kt
+++ app/src/main/java/kztproject/jp/splacounter/BaseActivity.kt
@@ -7,7 +7,6 @@ import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
-import kztproject.jp.splacounter.ui_common.replaceFragment
 import javax.inject.Inject
 
 class BaseActivity : AppCompatActivity(), HasSupportFragmentInjector {
@@ -19,7 +18,6 @@ class BaseActivity : AppCompatActivity(), HasSupportFragmentInjector {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
-        replaceFragment(R.id.container, SplashFragment.newInstance())
     }
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment>? {
--- app/src/main/java/kztproject/jp/splacounter/SplashFragment.kt
+++ app/src/main/java/kztproject/jp/splacounter/SplashFragment.kt
@@ -5,23 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import kotlinx.coroutines.*
-import kztproject.jp.splacounter.auth.ui.AuthFragment
-import kztproject.jp.splacounter.reward.list.ui.RewardListFragment
-import kztproject.jp.splacounter.ui_common.replaceFragmentWithNoStack
 import project.seito.screen_transition.preference.PrefsWrapper
 
 class SplashFragment : Fragment() {
-
-    companion object {
-        fun newInstance(): Fragment {
-            return SplashFragment()
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_splash, container, false)
@@ -44,9 +32,9 @@ class SplashFragment : Fragment() {
 
         val prefsWrapper = PrefsWrapper(context)
         if (prefsWrapper.userId != 0L) {
-            replaceFragmentWithNoStack(R.id.container, RewardListFragment.newInstance())
+            findNavController().navigate(SplashFragmentDirections.toRewardListFragment())
         } else {
-            replaceFragmentWithNoStack(R.id.container, AuthFragment.newInstance())
+            findNavController().navigate(SplashFragmentDirections.toAuthFragment())
         }
     }
 }
