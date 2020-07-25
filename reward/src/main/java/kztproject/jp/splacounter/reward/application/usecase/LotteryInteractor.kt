4e26122df22f4deb6d5373616db3f3e14988fd19
kseito <torinokonoko@gmail.com>
2020-07-25 15:18:04 +0900

    add lottery logic

--- reward/src/main/java/kztproject/jp/splacounter/reward/application/usecase/LotteryInteractor.kt
+++ reward/src/main/java/kztproject/jp/splacounter/reward/application/usecase/LotteryInteractor.kt
@@ -2,9 +2,18 @@ package kztproject.jp.splacounter.reward.application.usecase
 
 import kztproject.jp.splacounter.reward.database.model.Reward
 import javax.inject.Inject
+import kotlin.math.roundToInt
+import kotlin.random.Random
 
 class LotteryInteractor @Inject constructor() : LotteryUseCase {
     override suspend fun execute(rewards: List<Reward>): Int? {
-        return 0
+        rewards.forEach {
+            val denominator: Int = (100 / it.probability).roundToInt()
+            val result = Random.nextInt(denominator)
+            if (result == 0) {
+                return it.id
+            }
+        }
+        return null
     }
 }
