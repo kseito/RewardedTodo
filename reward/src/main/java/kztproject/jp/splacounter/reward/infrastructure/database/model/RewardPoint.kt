0c1def02c1643627c11e37670036ecab01ce3b29
k-seito <torinokonoko@gmail.com>
2020-08-10 17:41:04 +0900

    fix unit test

--- reward/src/main/java/kztproject/jp/splacounter/reward/infrastructure/database/model/RewardPoint.kt
+++ reward/src/main/java/kztproject/jp/splacounter/reward/infrastructure/database/model/RewardPoint.kt
@@ -1,4 +1,6 @@
 package kztproject.jp.splacounter.reward.infrastructure.database.model
 
+import com.google.gson.annotations.SerializedName
+
 @Suppress("EXPERIMENTAL_FEATURE_WARNING")
-inline class RewardPoint(val value: Int)
+inline class RewardPoint(@SerializedName("point") val value: Int)
