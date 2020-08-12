76a918dc75e76aabf92c6ebde382c288c25219b8
k-seito <torinokonoko@gmail.com>
2020-08-12 16:21:07 +0900

    fix unit test

--- reward/src/main/java/kztproject/jp/splacounter/reward/domain/model/TestRewardCreator.kt
+++ reward/src/main/java/kztproject/jp/splacounter/reward/domain/model/TestRewardCreator.kt
@@ -1,20 +1,43 @@
 package kztproject.jp.splacounter.reward.domain.model
 
-import kztproject.jp.splacounter.reward.infrastructure.database.model.RewardEntity
-
 object TestRewardCreator {
 
-    fun createOne(): List<RewardEntity> {
+    fun createOne(): List<Reward> {
         return listOf(
-                RewardEntity(1, "reward1", 1, 50F, null, false)
+                Reward(
+                        RewardId(1),
+                        "reward1",
+                        1,
+                        Probability(50F),
+                        null,
+                        false)
         )
     }
 
-    fun createThree(): List<RewardEntity> {
+    fun createThree(): List<Reward> {
         return listOf(
-                RewardEntity(1, "reward1", 1, 50F, null, false),
-                RewardEntity(2, "reward2", 1, 1F, null, false),
-                RewardEntity(3, "reward3", 1, 0.01F, null, false)
+                Reward(
+                        RewardId(1),
+                        "reward1",
+                        1,
+                        Probability(50F),
+                        null,
+                        false),
+
+                Reward(
+                        RewardId(2),
+                        "reward2",
+                        1,
+                        Probability(1F),
+                        null,
+                        false),
+                Reward(
+                        RewardId(3),
+                        "reward3",
+                        1,
+                        Probability(0.01F),
+                        null,
+                        false)
         )
     }
 }
--- reward/src/test/java/kztproject/jp/splacounter/reward/domain/model/LotteryBoxFactoryTest.kt
+++ reward/src/test/java/kztproject/jp/splacounter/reward/domain/model/LotteryBoxFactoryTest.kt
@@ -7,14 +7,14 @@ class LotteryBoxFactoryTest {
 
     @Test
     fun NoRewards() {
-        val actual = LotteryBoxFactory.create(listOf())
+        val actual = LotteryBoxFactory.create(RewardCollection(listOf()))
         assertThat(actual.tickets.size).isEqualTo(10000)
     }
 
     @Test
     fun OneReward() {
         val rewards = TestRewardCreator.createOne()
-        val lotteryBox = LotteryBoxFactory.create(rewards)
+        val lotteryBox = LotteryBoxFactory.create(RewardCollection(rewards))
         val prizeTickets = lotteryBox.tickets.filterIsInstance<Ticket.Prize>()
         assertThat(prizeTickets.size).isEqualTo(5000)
         prizeTickets.forEach {
@@ -26,7 +26,7 @@ class LotteryBoxFactoryTest {
     fun ThreeRewards() {
         val rewards = TestRewardCreator.createThree()
 
-        val lotteryBox = LotteryBoxFactory.create(rewards)
+        val lotteryBox = LotteryBoxFactory.create(RewardCollection(rewards))
 
         val firstRewardTickets = lotteryBox.tickets.filter { it is Ticket.Prize && it.rewardId == 1 }
         assertThat(firstRewardTickets.size).isEqualTo(5000)
