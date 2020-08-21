6f5944b5a243cb1ebd4bb3c5f1f39c8502171544
k-seito <torinokonoko@gmail.com>
2020-08-21 17:28:26 +0900

    add unit test to DeleteRewardInteractor

---
+++ reward/src/test/java/kztproject/jp/splacounter/reward/application/usecase/DeleteRewardInteractorTest.kt
@@ -0,0 +1,27 @@
+package kztproject.jp.splacounter.reward.application.usecase
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
+import kztproject.jp.splacounter.DummyCreator
+import kztproject.jp.splacounter.reward.infrastructure.database.RewardDao
+import org.junit.Test
+
+class DeleteRewardInteractorTest {
+
+    private val mockRewardDao: RewardDao = mock()
+    private val interactor = DeleteRewardInteractor(mockRewardDao)
+
+    @Test
+    fun canExecute() {
+        runBlocking {
+            val dummyReward = DummyCreator.createDummyReward()
+
+            interactor.execute(dummyReward)
+
+            verify(mockRewardDao, times(1)).deleteReward(any())
+        }
+    }
+}
