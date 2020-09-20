c8fe2d67a66e4c2cdc442fa42a622ded88b80515
kseito <torinokonoko@gmail.com>
2020-09-20 11:02:16 +0900

    associate ViewModel with Activity

--- app/src/main/java/kztproject/jp/splacounter/di/BaseActivityModule.kt
+++ app/src/main/java/kztproject/jp/splacounter/di/BaseActivityModule.kt
@@ -1,13 +1,24 @@
 package kztproject.jp.splacounter.di
 
+import androidx.lifecycle.ViewModel
+import dagger.Binds
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import dagger.multibindings.IntoMap
 import kztproject.jp.splacounter.presentation.HomeActivity
 import kztproject.jp.splacounter.auth.di.AuthModule
+import kztproject.jp.splacounter.presentation.HomeViewModel
 import kztproject.jp.splacounter.reward.di.RewardModule
+import kztproject.jp.splacounter.reward.presentation.list.RewardListViewModel
+import project.seito.screen_transition.di.ViewModelKey
 
 @Module
 internal abstract class BaseActivityModule {
     @ContributesAndroidInjector(modules = [RewardModule::class, AuthModule::class])
     internal abstract fun contributeBaseActivity(): HomeActivity
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(HomeViewModel::class)
+    abstract fun bindHomeViewModel(viewModel: HomeViewModel): ViewModel
 }
--- app/src/main/java/kztproject/jp/splacounter/presentation/HomeActivity.kt
+++ app/src/main/java/kztproject/jp/splacounter/presentation/HomeActivity.kt
@@ -3,11 +3,13 @@ package kztproject.jp.splacounter.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import kztproject.jp.splacounter.R
+import kztproject.jp.splacounter.reward.presentation.list.RewardListViewModel
 import javax.inject.Inject
 
 class HomeActivity : AppCompatActivity(), HasSupportFragmentInjector {
@@ -15,10 +17,15 @@ class HomeActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    lateinit var viewModel: HomeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(HomeViewModel::class.java)
     }
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment>? {
