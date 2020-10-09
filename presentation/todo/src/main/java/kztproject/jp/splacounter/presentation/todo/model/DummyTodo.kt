fcb33269bc1d44f221599b8dc72a909a1403bba9
kseito <torinokonoko@gmail.com>
2020-10-10 07:03:24 +0900

    add Todo parameter

--- presentation/todo/src/main/java/kztproject/jp/splacounter/presentation/todo/model/DummyTodo.kt
+++ presentation/todo/src/main/java/kztproject/jp/splacounter/presentation/todo/model/DummyTodo.kt
@@ -1,5 +1,8 @@
 package kztproject.jp.splacounter.presentation.todo.model
 
 data class DummyTodo(
-        val name: String
+        val id: Long,
+        val name: String,
+        val numberOfTicketsObtained: Float,
+        val isRepeat: Boolean
 )
