2da878da9c5eb1f5a2f02e19e4a4abd0bef04a25
kseito <torinokonoko@gmail.com>
2020-10-17 07:23:02 +0900

    add annotation

--- data/todo/src/main/java/kztproject/jp/splacounter/data/todo/AppDatabase.kt
+++ data/todo/src/main/java/kztproject/jp/splacounter/data/todo/AppDatabase.kt
@@ -1,7 +1,9 @@
 package kztproject.jp.splacounter.data.todo
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
 
+@Database(entities = [TodoEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun todoDao(): TodoDao
 }
