7fc779e85715a81ca06ed10aa2a818cd0f464218
kseito <torinokonoko@gmail.com>
2020-10-17 05:10:09 +0900

    apply usecase to viewmodel

--- presentation/todo/src/main/java/kztproject/jp/splacounter/presentation/todo/TodoListViewModel.kt
+++ presentation/todo/src/main/java/kztproject/jp/splacounter/presentation/todo/TodoListViewModel.kt
@@ -3,19 +3,23 @@ package kztproject.jp.splacounter.presentation.todo
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import kztproject.jp.splacounter.todo.application.GetTodoListUseCase
 import kztproject.jp.splacounter.todo.domain.Todo
 import javax.inject.Inject
 
-class TodoListViewModel @Inject constructor() : ViewModel() {
+class TodoListViewModel @Inject constructor(
+        private val getTodoListUseCase: GetTodoListUseCase
+) : ViewModel() {
 
     private val todoList: MutableLiveData<List<Todo>> = MutableLiveData()
 
     fun loadTodo() {
-        val todoList = listOf(1, 2, 3, 4, 5)
-                .map {
-                    Todo(it.toLong(), "Test Todo $it", 0.5f, true)
-                }
-        this.todoList.value = todoList
+        viewModelScope.launch {
+            val newTodoList = getTodoListUseCase.execute()
+            todoList.value = newTodoList
+        }
     }
 
     fun observeTodo(): LiveData<List<Todo>> {
