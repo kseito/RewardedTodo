83232e7e1be893c9ad9875f1cb18135ee11c816f
kseito <torinokonoko@gmail.com>
2020-11-07 07:41:43 +0900

    add list divider

--- presentation/todo/src/main/java/kztproject/jp/splacounter/presentation/todo/TodoListFragment.kt
+++ presentation/todo/src/main/java/kztproject/jp/splacounter/presentation/todo/TodoListFragment.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.android.support.AndroidSupportInjection
@@ -51,9 +52,11 @@ class TodoListFragment : Fragment(), TodoListViewAdapter.OnItemClickListener, To
     }
 
     private fun initTodoListViewAdapter() {
-        binding.todoListView.layoutManager = LinearLayoutManager(context)
+        val layoutManager = LinearLayoutManager(context)
+        binding.todoListView.layoutManager = layoutManager
         adapter.setListener(this)
         binding.todoListView.adapter = adapter
+        binding.todoListView.addItemDecoration(DividerItemDecoration(context, layoutManager.orientation))
         viewModel.todoList.observe(viewLifecycleOwner, Observer { todoList ->
             adapter.setTodo(todoList)
         })
