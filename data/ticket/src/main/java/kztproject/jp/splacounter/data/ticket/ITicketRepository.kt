87c2daeaf6c515a7d23e203d4149e8647b75e9d8
kseito <torinokonoko@gmail.com>
2020-11-23 16:00:04 +0900

    consume ticket when executing lottery

--- data/ticket/src/main/java/kztproject/jp/splacounter/data/ticket/ITicketRepository.kt
+++ data/ticket/src/main/java/kztproject/jp/splacounter/data/ticket/ITicketRepository.kt
@@ -3,6 +3,8 @@ package kztproject.jp.splacounter.data.ticket
 interface ITicketRepository {
     fun addTicket(numberOfTicket: Float)
 
+    fun consumeTicket()
+
     //TODO change return type to domain model
     fun getNumberOfTicket(): Float
 }
--- data/ticket/src/main/java/kztproject/jp/splacounter/data/ticket/TicketRepository.kt
+++ data/ticket/src/main/java/kztproject/jp/splacounter/data/ticket/TicketRepository.kt
@@ -7,12 +7,21 @@ class TicketRepository @Inject constructor(private val preferences: SharedPrefer
 
     companion object {
         private const val NUMBER_OF_TICKET = "number_of_ticket"
+        //TODO put into domain layer
+        private const val NUMBER_OF_TICKETS_REQUIRED_FOR_LOTTERY = 1
     }
 
     override fun addTicket(numberOfTicket: Float) {
         val current = getNumberOfTicket()
         preferences.edit()
                 .putFloat(NUMBER_OF_TICKET, current + numberOfTicket)
+                .apply()
+    }
+
+    override fun consumeTicket() {
+        val current = getNumberOfTicket()
+        preferences.edit()
+                .putFloat(NUMBER_OF_TICKET, current - NUMBER_OF_TICKETS_REQUIRED_FOR_LOTTERY)
                 .apply()
     }
 
