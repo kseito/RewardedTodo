54db7e09e8795363f7fdfeab944c10f89bf7019b
kseito <torinokonoko@gmail.com>
2020-11-27 05:53:50 +0900

    add Kotlin DSL

---
+++ buildSrc/src/main/java/Packages.kt
@@ -0,0 +1,4 @@
+@Suppress("unused")
+object Packages {
+    const val name = "kztproject.jp.splacounter"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,9 @@
+@Suppress("unused")
+object Versions {
+    const val androidTargetSdkVersion = 28
+    const val androidCompileSdkVersion = 28
+    const val androidMinSdkVersion = 21
+
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
