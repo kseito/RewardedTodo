8500d28bf8c376881b3ee2315326a004f706d0a7
kseito <torinokonoko@gmail.com>
2020-11-30 20:35:15 +0900

    update Kotlin version

--- buildSrc/src/main/java/dependencies/GradlePlugins.kt
+++ buildSrc/src/main/java/dependencies/GradlePlugins.kt
@@ -5,7 +5,7 @@ object GradlePlugins {
         const val version = "4.1.1"
     }
     object Kotlin {
-        const val version = "1.3.72"
+        const val version = "1.4.20"
     }
     const val android = "com.android.tools.build:gradle:${Android.version}"
     const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
