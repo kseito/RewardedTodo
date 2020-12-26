7aa614a48a43216881f46fc0b6c0bf016dcdc9ae
kazuto.seito <>
2020-12-27 08:39:21 +0900

    supress warnings

--- domain/reward/src/main/java/jp/kztproject/rewardedtodo/domain/reward/LotteryBox.kt
+++ domain/reward/src/main/java/jp/kztproject/rewardedtodo/domain/reward/LotteryBox.kt
@@ -1,7 +1,5 @@
 package jp.kztproject.rewardedtodo.domain.reward
 
-import jp.kztproject.rewardedtodo.domain.reward.Ticket
-
 class LotteryBox(val tickets: List<Ticket>) {
 
     fun draw(ticketNumber: Int): Ticket {
--- domain/reward/src/main/java/jp/kztproject/rewardedtodo/domain/reward/RewardId.kt
+++ domain/reward/src/main/java/jp/kztproject/rewardedtodo/domain/reward/RewardId.kt
@@ -1,3 +1,4 @@
 package jp.kztproject.rewardedtodo.domain.reward
 
+@Suppress("EXPERIMENTAL_FEATURE_WARNING")
 inline class RewardId(val value: Int)
