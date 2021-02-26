bde8d91a702b527e561b3153ce6300f77d5f2883
kazuto.seito <torinokonoko@gmail.com>
2021-02-26 18:36:07 +0900

    create repository

---
+++ data/auth/src/main/java/jp/kztproject/rewardedtodo/data/auth/TodoistAccessTokenRepository.kt
@@ -0,0 +1,8 @@
+package jp.kztproject.rewardedtodo.data.auth
+
+class TodoistAccessTokenRepository {
+    fun fetch(clientId: String, clientToken: String, code: String): String {
+        //TODO get access token from Todoist
+        return ""
+    }
+}
