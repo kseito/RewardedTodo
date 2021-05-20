67e708fdc06cb81aa3159de5b442c55ee203fb82
kazuto.seito <torinokonoko@gmail.com>
2021-05-20 22:05:16 +0900

    require access token

--- presentation/auth/src/main/java/jp/kztproject/rewardedtodo/presentation/auth/todoist/TodoistAuthActivity.kt
+++ presentation/auth/src/main/java/jp/kztproject/rewardedtodo/presentation/auth/todoist/TodoistAuthActivity.kt
@@ -10,33 +10,46 @@ import jp.kztproject.rewardedtodo.presentation.auth.R
 import jp.kztproject.rewardedtodo.presentation.auth.databinding.ActivityTodoistAuthBinding
 import javax.inject.Inject
 
-class TodoistAuthActivity: AppCompatActivity(), TodoistAuthWebViewClient.AuthResultListener {
+class TodoistAuthActivity : AppCompatActivity(), TodoistAuthWebViewClient.AuthResultListener, TodoistAuthViewModel.Callback {
 
-    private lateinit var binding : ActivityTodoistAuthBinding
+    private lateinit var binding: ActivityTodoistAuthBinding
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: TodoistAuthViewModel
+
+    private val clientId by lazy {
+        getString(R.string.todoist_client_id)
+    }
+    private val clientSecret by lazy {
+        getString(R.string.todoist_client_secret)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
 
         viewModel = ViewModelProvider(this, viewModelFactory).get(TodoistAuthViewModel::class.java)
+        viewModel.callback = this
         binding = DataBindingUtil.setContentView(this, R.layout.activity_todoist_auth)
 
-        val clientId = getString(R.string.todoist_client_id)
-        val clientSecret = getString(R.string.todoist_client_secret)
         binding.webView.loadUrl("https://todoist.com/oauth/authorize?client_id=$clientId&scope=data:read&state=$clientSecret")
         binding.webView.webViewClient = TodoistAuthWebViewClient(this)
     }
 
     override fun onAuthSuccess(code: String) {
-        //TODO notify auth success
-        finish()
+        viewModel.requireAccessToken(clientId, clientSecret, code)
     }
 
     override fun onAuthFailure() {
+        Toast.makeText(this, "Authorization failed", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRequireAccessTokenSuccess(accessToken: String) {
+        Toast.makeText(this, "My token is $accessToken", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRequireAccessTokenFailed() {
         Toast.makeText(this, "Authorization failed", Toast.LENGTH_LONG).show()
     }
 }
--- presentation/auth/src/main/java/jp/kztproject/rewardedtodo/presentation/auth/todoist/TodoistAuthViewModel.kt
+++ presentation/auth/src/main/java/jp/kztproject/rewardedtodo/presentation/auth/todoist/TodoistAuthViewModel.kt
@@ -4,15 +4,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import jp.kztproject.rewardedtodo.data.auth.ITodoistAccessTokenRepository
 import kotlinx.coroutines.launch
+import java.lang.Exception
 import javax.inject.Inject
 
 class TodoistAuthViewModel @Inject constructor(
         private val todoistAccessTokenRepository: ITodoistAccessTokenRepository
 ) : ViewModel() {
 
-    fun requireAccessToken() {
+    interface Callback {
+        fun onRequireAccessTokenSuccess(accessToken: String)
+
+        fun onRequireAccessTokenFailed()
+    }
+    lateinit var callback: Callback
+
+    fun requireAccessToken(clientId: String, clientSecret: String, code: String) {
         viewModelScope.launch {
-            //TODO use TodoistAccessTokenRepository
+            try {
+                val accessToken = todoistAccessTokenRepository.fetch(clientId, clientSecret, code)
+                callback.onRequireAccessTokenSuccess(accessToken)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                callback.onRequireAccessTokenFailed()
+            }
         }
     }
 }
