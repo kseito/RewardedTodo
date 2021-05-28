390a7cd05199c89fbb93d12b523e2a95eb3d0719
kazuto.seito <torinokonoko@gmail.com>
2021-05-28 18:30:15 +0900

    add EncryptedSharedPreferences to constructor

--- data/auth/src/main/java/jp/kztproject/rewardedtodo/data/auth/TodoistAccessTokenRepository.kt
+++ data/auth/src/main/java/jp/kztproject/rewardedtodo/data/auth/TodoistAccessTokenRepository.kt
@@ -1,9 +1,12 @@
 package jp.kztproject.rewardedtodo.data.auth
 
+import android.content.SharedPreferences
 import javax.inject.Inject
+import javax.inject.Named
 
 class TodoistAccessTokenRepository @Inject constructor(
-        private val api: TodoistApi
+        private val api: TodoistApi,
+        @Named("encrypted") private val preferences: SharedPreferences
 ) : ITodoistAccessTokenRepository {
 
     override suspend fun fetch(clientId: String, clientToken: String, code: String): String {
