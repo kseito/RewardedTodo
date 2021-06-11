71a9a3f689a4cad4edefece475cf0983cd78649b
kazuto.seito <>
2021-06-12 06:13:12 +0900

    remove dagger.android from Application

--- app/src/main/java/jp/kztproject/rewardedtodo/MyApplication.kt
+++ app/src/main/java/jp/kztproject/rewardedtodo/MyApplication.kt
@@ -2,17 +2,10 @@ package jp.kztproject.rewardedtodo
 
 import android.app.Application
 import com.facebook.stetho.Stetho
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
 
 @HiltAndroidApp
-class MyApplication : Application(), HasAndroidInjector {
-
-    @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
@@ -20,9 +13,5 @@ class MyApplication : Application(), HasAndroidInjector {
         if (BuildConfig.DEBUG) {
             Stetho.initializeWithDefaults(this)
         }
-    }
-
-    override fun androidInjector(): AndroidInjector<Any> {
-        return dispatchingAndroidInjector
     }
 }
