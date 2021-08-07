b61a3eb34b772a050186c04ce636efef564f644d
kseito <torinokonoko@gmail.com>
2021-08-08 07:38:53 +0900

    check danger is working

--- presentation/auth/src/main/java/jp/kztproject/rewardedtodo/presentation/auth/todoist/TodoistAuthActivity.kt
+++ presentation/auth/src/main/java/jp/kztproject/rewardedtodo/presentation/auth/todoist/TodoistAuthActivity.kt
@@ -1,6 +1,7 @@
 package jp.kztproject.rewardedtodo.presentation.auth.todoist
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
