e805db217e328c26b7a63e2457dbb9481147a3a7
Kazuto Seito <kazuto.seito@studist.jp>
2021-08-25 18:14:20 +0900

    change method parameter

--- common/database/src/main/java/jp/kztproject/rewardedtodo/common/database/DatabaseInitializer.kt
+++ common/database/src/main/java/jp/kztproject/rewardedtodo/common/database/DatabaseInitializer.kt
@@ -1,12 +1,12 @@
 package jp.kztproject.rewardedtodo.common.database
 
-import android.app.Application
+import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
 object DatabaseInitializer {
-    fun <T : RoomDatabase> init(application: Application, cls: Class<T>, name: String): T {
-        return Room.databaseBuilder(application, cls, "rewardedtodo_$name")
+    fun <T : RoomDatabase> init(context: Context, cls: Class<T>, name: String): T {
+        return Room.databaseBuilder(context, cls, "rewardedtodo_$name")
                 .fallbackToDestructiveMigration()
                 .build()
     }
