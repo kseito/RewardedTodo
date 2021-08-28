2c63e5b5afa3521e4c927845d93d2e0ccb187422
kseito <torinokonoko@gmail.com>
2021-08-28 09:29:54 +0900

    execute ktlint on Danger

--- buildSrc/src/main/java/dependencies/GradlePlugins.kt
+++ buildSrc/src/main/java/dependencies/GradlePlugins.kt
@@ -10,8 +10,12 @@ object GradlePlugins {
     object Hilt {
         const val version = "2.37"
     }
+    object KtlintGradle {
+        const val version = "10.1.0"
+    }
     const val android = "com.android.tools.build:gradle:${Android.version}"
     const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
     const val navigation_safe_args = "androidx.navigation:navigation-safe-args-gradle-plugin:${Libraries.AndroidX.NavigationComponent.version}"
     const val dagger_hilt = "com.google.dagger:hilt-android-gradle-plugin:${Hilt.version}"
+    const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:${KtlintGradle.version}"
 }
