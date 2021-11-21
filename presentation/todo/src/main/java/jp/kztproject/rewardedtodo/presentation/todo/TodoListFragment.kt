7918e54745df595fda8cfd94ea11647e09797b63
Kazuto Seito <kazuto.seito@studist.jp>
2021-11-21 14:12:11 +0900

    introduce jetpack compose

--- presentation/todo/src/main/java/jp/kztproject/rewardedtodo/presentation/todo/TodoListFragment.kt
+++ presentation/todo/src/main/java/jp/kztproject/rewardedtodo/presentation/todo/TodoListFragment.kt
@@ -5,14 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
+import jp.kztproject.rewardedtodo.presentation.reward.list.RewardedTodoScheme
 import jp.kztproject.rewardedtodo.presentation.todo.databinding.FragmentTodoListBinding
 import jp.kztproject.rewardedtodo.presentation.todo.databinding.ViewTodoDetailBinding
 import jp.kztproject.rewardedtodo.presentation.todo.model.EditingTodo
@@ -36,31 +38,40 @@ class TodoListFragment : Fragment(), TodoListViewAdapter.OnItemClickListener, To
         viewModel.initialize(this)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentTodoListBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MaterialTheme(
+                    colors = RewardedTodoScheme(isSystemInDarkTheme())
+                ) {
+                    Text(
+                        text = "Hello Todo List"
+                    )
+                }
+            }
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        initTodoListViewAdapter()
-
-        binding.addButton.setOnClickListener {
-            showTodoDetail(EditingTodo())
-        }
+//        initTodoListViewAdapter()
+//
+//        binding.addButton.setOnClickListener {
+//            showTodoDetail(EditingTodo())
+//        }
     }
 
-    private fun initTodoListViewAdapter() {
-        val layoutManager = LinearLayoutManager(context)
-        adapter.setListener(this)
-        binding.todoListView.apply {
-            this.layoutManager = layoutManager
-            addItemDecoration(DividerItemDecoration(context, layoutManager.orientation))
-            this.adapter = this@TodoListFragment.adapter
-        }
-        viewModel.todoList.observe(viewLifecycleOwner, Observer { todoList ->
-            adapter.setTodo(todoList)
-        })
-    }
+//    private fun initTodoListViewAdapter() {
+//        val layoutManager = LinearLayoutManager(context)
+//        adapter.setListener(this)
+//        binding.todoListView.apply {
+//            this.layoutManager = layoutManager
+//            addItemDecoration(DividerItemDecoration(context, layoutManager.orientation))
+//            this.adapter = this@TodoListFragment.adapter
+//        }
+//        viewModel.todoList.observe(viewLifecycleOwner, Observer { todoList ->
+//            adapter.setTodo(todoList)
+//        })
+//    }
 
     override fun onClick(item: Todo) {
         val editingTodo = EditingTodo.from(item)
@@ -74,7 +85,7 @@ class TodoListFragment : Fragment(), TodoListViewAdapter.OnItemClickListener, To
     private fun showTodoDetail(item: EditingTodo) {
         val bottomSheet = BottomSheetDialog(requireContext())
         val binding = DataBindingUtil.inflate<ViewTodoDetailBinding>(
-                LayoutInflater.from(context), R.layout.view_todo_detail, this.binding.root as ViewGroup, false
+            LayoutInflater.from(context), R.layout.view_todo_detail, this.binding.root as ViewGroup, false
         )
         bottomSheet.setContentView(binding.root)
         binding.todo = item
