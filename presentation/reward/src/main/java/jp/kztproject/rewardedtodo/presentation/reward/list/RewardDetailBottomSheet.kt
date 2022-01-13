565f3a1c627c4811a4c70e42d82fe5a2c3326584
Kazuto Seito <kazuto.seito@studist.jp>
2022-01-13 16:59:18 +0900

    add new reward detail screen

--- presentation/reward/src/main/java/jp/kztproject/rewardedtodo/presentation/reward/list/RewardDetailBottomSheet.kt
+++ presentation/reward/src/main/java/jp/kztproject/rewardedtodo/presentation/reward/list/RewardDetailBottomSheet.kt
@@ -1,11 +1,15 @@
 package jp.kztproject.rewardedtodo.presentation.reward.list
 
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @ExperimentalMaterialApi
 @Composable
@@ -26,18 +30,60 @@ fun RewardDetailBottomSheet(
 @ExperimentalMaterialApi
 @Composable
 private fun RewardDetailBottomSheetContent() {
-    // TODO show content
-    LazyColumn {
-        items(50) {
-            ListItem(
-                text = { Text("Item $it") },
-                icon = {
-                    Icon(
-                        Icons.Default.Favorite,
-                        contentDescription = "Localized description"
-                    )
-                }
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+    var chanceOfWinning by remember { mutableStateOf("") }
+    var repeat by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+    ) {
+        val modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+        TextField(
+            value = title,
+            onValueChange = { title = it },
+            modifier = modifier
+        )
+        TextField(
+            value = description,
+            onValueChange = { description = it },
+            modifier = modifier
+        )
+        TextField(
+            value = chanceOfWinning,
+            onValueChange = { chanceOfWinning = it },
+            modifier = modifier
+        )
+        Row(
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+        ) {
+            Checkbox(
+                checked = repeat,
+                onCheckedChange = { repeat = it }
             )
+            Text(
+                text = "Repeat",
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+            )
+        }
+        Row {
+            Button(
+                modifier = Modifier
+                    .padding(end = 8.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Text("Save")
+            }
+            Button(
+                onClick = { /*TODO*/ }
+            ) {
+                Text("Delete")
+            }
         }
     }
 }
