64b8fc2baab3b3d121453a9709c2c31ca4ea6ad6
Kazuto Seito <torinokonoko@gmail.com>
2022-04-16 08:19:50 +0900

    update AGP

--- buildSrc/src/main/java/dependencies/GradlePlugins.kt
+++ buildSrc/src/main/java/dependencies/GradlePlugins.kt
@@ -2,7 +2,7 @@ package dependencies
 
 object GradlePlugins {
     object Android {
-        const val version = "7.1.2"
+        const val version = "7.1.3"
     }
     object Kotlin {
         const val version = "1.6.10"
