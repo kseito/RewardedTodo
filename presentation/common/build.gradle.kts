ae41872bbc796cd81258bec0be0bc366e0569328
Kazuto Seito <torinokonoko@gmail.com>
2022-04-29 07:21:07 +0900

    add a new module

---
+++ presentation/common/build.gradle.kts
@@ -0,0 +1,20 @@
+import dependencies.Libraries
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+apply(from = rootProject.file("gradle/android_common.gradle"))
+
+dependencies {
+
+    // Jetpack Compose
+    implementation(Libraries.AndroidX.activityCompose)
+    implementation(Libraries.AndroidX.Compose.foundation)
+    implementation(Libraries.AndroidX.Compose.material)
+    implementation(Libraries.AndroidX.Compose.ui)
+    implementation(Libraries.AndroidX.Compose.uiTooling)
+    implementation(Libraries.AndroidX.Compose.liveData)
+    implementation(Libraries.AndroidX.Compose.constraintLayout)
+}
