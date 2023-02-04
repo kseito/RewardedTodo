4b5067826b22c54fcb77c7afcf16d77be27f5cc6
Kazuto Seito <torinokonoko@gmail.com>
2023-02-04 12:53:47 +0900

    Show Text

--- app/src/main/java/jp/kztproject/rewardedtodo/presentation/HomeActivity.kt
+++ app/src/main/java/jp/kztproject/rewardedtodo/presentation/HomeActivity.kt
@@ -1,41 +1,25 @@
 package jp.kztproject.rewardedtodo.presentation
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 import dagger.hilt.android.AndroidEntryPoint
-import jp.kztproject.rewardedtodo.R
-import jp.kztproject.rewardedtodo.databinding.ActivityHomeBinding
 import project.seito.screen_transition.IFragmentsTransitionManager
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class HomeActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityHomeBinding
-    private lateinit var appBarConfiguration: AppBarConfiguration
+class HomeActivity : ComponentActivity() {
 
     @Inject
     lateinit var fragmentTransitionManager: IFragmentsTransitionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
 
-//        setSupportActionBar(binding.toolbar)
-//
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-//        val navController = navHostFragment.navController
-//        appBarConfiguration = AppBarConfiguration(setOf(R.id.todo_list_fragment, R.id.reward_list_fragment), binding.drawerLayout)
-//
-//        //FIXME Navigation drawer menu not working
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        binding.navigationView.setupWithNavController(navController)
+        setContent {
+            // TODO Use NavHost
+            Text("Hello world!")
+        }
     }
-
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.nav_host_fragment)
-//        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-//    }
 }
