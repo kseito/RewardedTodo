077cd273e0972010af2e028cd7a68f123db5ca29
Kazuto Seito <torinokonoko@gmail.com>
2023-02-05 16:17:49 +0900

    Collect unit test result by jacoco

---
+++ feature/todo/src/test/java/jp/kztproject/rewardedtodo/todo/application/UpdateTodoInteractorTest.kt
@@ -0,0 +1,13 @@
+package jp.kztproject.rewardedtodo.todo.application
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class UpdateTodoInteractorTest {
+
+    @Test
+    fun test() {
+        // TODO write validate test
+        assertTrue(1 + 1 == 2)
+    }
+}
