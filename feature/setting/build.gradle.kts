e3219437c312c97dfd657f32f0ba5b3e164e199d
Kazuto Seito <torinokonoko@gmail.com>
2023-02-11 09:39:53 +0900

    Add SettingDialog

--- feature/setting/build.gradle.kts
+++ feature/setting/build.gradle.kts
@@ -13,10 +13,28 @@ dependencies {
     implementation(Libraries.AndroidX.coreKtx)
     implementation(Libraries.AndroidX.appCompat)
     implementation(Libraries.AndroidX.design)
+    implementation(Libraries.AndroidX.activityCompose)
+    implementation(Libraries.AndroidX.Compose.foundation)
+    implementation(Libraries.AndroidX.Compose.material)
+    implementation(Libraries.AndroidX.Compose.material3)
+    implementation(Libraries.AndroidX.Compose.ui)
+    implementation(Libraries.AndroidX.Compose.uiTooling)
+    implementation(Libraries.AndroidX.Compose.liveData)
+    implementation(Libraries.AndroidX.Compose.constraintLayout)
+    implementation(Libraries.AndroidX.Compose.hiltNavigationCompose)
 
     implementation(Libraries.Test.junit)
 }
 
 android {
+    buildFeatures {
+        dataBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Libraries.AndroidX.Compose.compilerVersion
+    }
+
     namespace = "jp.kztproject.rewardedtodo.feature.setting"
 }
---
+++ feature/setting/src/main/java/jp/kztproject/rewardedtodo/feature/setting/SettingDialog.kt
@@ -0,0 +1,102 @@
+package jp.kztproject.rewardedtodo.feature.setting
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Switch
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SettingDialog(
+    onDismiss: () -> Unit,
+    onTodoistClicked: () -> Unit
+) {
+    val configuration = LocalConfiguration.current
+
+    AlertDialog(
+        title = {
+            Text(
+                text = stringResource(R.string.settings_title),
+                style = MaterialTheme.typography.titleLarge,
+            )
+        },
+        text = {
+            Column {
+                Divider(Modifier.padding(vertical = 8.dp))
+                SettingDialogSectionTitle(text = stringResource(R.string.extensions_section))
+                SettingDialogTodoistExtensionRow(
+                    onTodoistClicked = onTodoistClicked
+                )
+            }
+        },
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            Text(
+                text = stringResource(android.R.string.ok),
+                style = MaterialTheme.typography.labelLarge,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable { onDismiss() },
+            )
+        },
+        modifier = Modifier.widthIn(max = configuration.screenWidthDp.dp - 80.dp)
+    )
+}
+
+@Composable
+private fun SettingDialogSectionTitle(text: String) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.titleMedium,
+        modifier = Modifier
+    )
+}
+
+@Composable
+private fun SettingDialogTodoistExtensionRow(
+    onTodoistClicked: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(
+                interactionSource =  remember { MutableInteractionSource() },
+                indication = rememberRipple(),
+                onClick = onTodoistClicked
+            )
+    ) {
+        Text(text = stringResource(R.string.todoist_extension_title))
+
+        Switch(
+            checked = false,
+            onCheckedChange = null,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+
+        )
+    }
+}
+
+@Composable
+@Preview
+fun SettingDialogPreview() {
+    SettingDialog(
+        onDismiss = {},
+        onTodoistClicked = {}
+    )
+}
