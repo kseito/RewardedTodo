f34a53859459e18a787cd2a9421dff9beb4ddaba
Kazuto Seito <torinokonoko@gmail.com>
2023-05-20 19:43:31 +0900

    Update build.gradle.kts

--- application/todo/build.gradle.kts
+++ application/todo/build.gradle.kts
@@ -1,17 +1,35 @@
+import dependencies.Libraries
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("org.jlleitschuh.gradle.ktlint")
+    kotlin("kapt")
 }
 
 apply(from = rootProject.file("gradle/android_common.gradle"))
 apply(from = rootProject.file("gradle/ktlint.gradle"))
 
+android {
+    namespace = "jp.kztproject.rewardedtodo.application.todo"
+}
+
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //TODO need to reverse dependencies when todo module separate application layer and domain layer
+    implementation(project(path = ":data:ticket"))
+
+    implementation(Libraries.Kotlin.stdlib)
+
+    implementation(Libraries.AndroidX.appCompat)
+    implementation(Libraries.AndroidX.coreKtx)
+
+    testImplementation(Libraries.Test.junit)
+
+    //Dagger
+    implementation(Libraries.Dagger.core)
+    kapt(Libraries.Dagger.compiler)
+
+    //Coroutines
+    implementation(Libraries.Kotlin.Coroutines.core)
 }
