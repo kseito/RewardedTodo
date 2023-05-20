c83d5ead4340f716493042d48f6090e99fa35a3c
Kazuto Seito <torinokonoko@gmail.com>
2023-05-20 10:31:02 +0900

    Add feature:reward module

---
+++ feature/reward/build.gradle.kts
@@ -0,0 +1,70 @@
+import dependencies.Libraries
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("dagger.hilt.android.plugin")
+    id("org.jlleitschuh.gradle.ktlint")
+    kotlin("kapt")
+}
+
+apply(from = rootProject.file("gradle/android_common.gradle"))
+
+android {
+    namespace = "jp.kztproject.rewardedtodo.presentation.reward"
+}
+
+dependencies {
+
+    implementation(Libraries.Kotlin.stdlib)
+    implementation(Libraries.AndroidX.coreKtx)
+    implementation(Libraries.AndroidX.appCompat)
+    implementation(Libraries.AndroidX.constraintLayout)
+    implementation(Libraries.AndroidX.recyclerView)
+    implementation(Libraries.AndroidX.design)
+    implementation(Libraries.AndroidX.LifeCycle.viewModelKtx)
+    implementation(Libraries.AndroidX.NavigationComponent.fragmentKtx)
+
+    // Jetpack Compose
+    implementation(Libraries.AndroidX.activityCompose)
+    implementation(Libraries.AndroidX.Compose.foundation)
+    implementation(Libraries.AndroidX.Compose.material)
+    implementation(Libraries.AndroidX.Compose.ui)
+    implementation(Libraries.AndroidX.Compose.uiTooling)
+    implementation(Libraries.AndroidX.Compose.liveData)
+    implementation(Libraries.AndroidX.Compose.constraintLayout)
+    implementation(Libraries.AndroidX.Compose.hiltNavigationCompose)
+
+    //Dagger
+    implementation(Libraries.Dagger.core)
+    annotationProcessor(Libraries.Dagger.compiler)
+    kapt(Libraries.Dagger.compiler)
+    implementation(Libraries.Dagger.hilt)
+    kapt(Libraries.Dagger.hiltCompiler)
+
+    //Coroutines
+    implementation(Libraries.Kotlin.Coroutines.core)
+    implementation(Libraries.Kotlin.Coroutines.android)
+    implementation(Libraries.Kotlin.Coroutines.adapter)
+
+    //Test
+    testImplementation(Libraries.Test.junit)
+    testImplementation(Libraries.Test.androidXCore)
+    testImplementation(Libraries.Test.mockito)
+    testImplementation(Libraries.Test.mockitoKotlin)
+    testImplementation(Libraries.Test.coroutinesTest)
+    testImplementation(Libraries.Test.mockWebServer)
+    testImplementation(Libraries.Test.robolectric)
+    testImplementation(Libraries.Test.assertJ)
+    testImplementation(Libraries.Test.AndroidX.coreTesting)
+    implementation(project(path = ":test:reward"))
+    androidTestImplementation(Libraries.Test.AndroidX.junit)
+    androidTestImplementation(Libraries.Test.AndroidX.espresso)
+
+    implementation(project(path = ":application:reward"))
+    implementation(project(path = ":domain:reward"))
+    implementation(project(path = ":common:ui"))
+    implementation(project(path = ":presentation:common"))
+}
+
+apply(from = rootProject.file("gradle/ktlint.gradle"))
