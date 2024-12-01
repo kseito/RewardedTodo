7ef3d92400c58bc80a7f1eb538ed4698e003b928
kseito <torinokonoko@gmail.com>
2024-12-01 16:09:05 +0900

    Migrate application/reward/build.gradle

---
+++ application/reward/build.gradle.kts
@@ -0,0 +1,45 @@
+import dependencies.GradlePlugins
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ktlint.gradle)
+}
+
+apply(from = rootProject.file("gradle/android_common.gradle"))
+
+dependencies {
+    implementation(libs.kotlin.stdlib)
+    implementation(libs.hilt.android)
+
+    // Coroutines
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+    implementation(libs.coroutines.adapter)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.assertj.core)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.coroutines.test)
+    testImplementation(libs.mockk)
+    testImplementation(project(":test:reward"))
+
+    implementation(project(":domain:reward"))
+    implementation(project(":data:ticket"))
+}
+
+android {
+    namespace = "jp.kztproject.rewardedtodo.application.reward"
+}
+
+configure<KtlintExtension> {
+    version.set(GradlePlugins.Ktlint.version)
+    android.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+    }
+    ignoreFailures.set(true)
+}
