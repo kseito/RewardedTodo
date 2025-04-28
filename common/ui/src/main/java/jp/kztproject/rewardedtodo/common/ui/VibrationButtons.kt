16c9345c7df9d99ec179f5e5691fe1c687fb0e18
kseito <torinokonoko@gmail.com>
2025-04-28 15:22:18 +0900

    add rich vibration effect to context

--- common/ui/src/main/java/jp/kztproject/rewardedtodo/common/ui/VibrationButtons.kt
+++ common/ui/src/main/java/jp/kztproject/rewardedtodo/common/ui/VibrationButtons.kt
@@ -13,3 +13,19 @@ fun Context.vibrate() {
     val combinedVibration = CombinedVibration.createParallel(vibratorEffect)
     vibratorManager.vibrate(combinedVibration)
 }
+
+@SuppressLint("MissingPermission")
+fun Context.vibrateRichly() {
+    val vibratorManager = getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+    val timings: LongArray = longArrayOf(
+        25, 25, 50, 25, 25, 25, 25, 25, 25, 25, 75, 25, 25,
+        300, 25, 25, 150, 25, 25, 25
+    )
+    val amplitudes: IntArray = intArrayOf(
+        38, 77, 79, 84, 92, 99, 121, 143, 180, 217, 255, 170, 85,
+        0, 85, 170, 255, 170, 85, 0
+    )
+    val newVibration = VibrationEffect.createWaveform(timings, amplitudes, -1)
+    val combinedVibration = CombinedVibration.createParallel(newVibration)
+    vibratorManager.vibrate(combinedVibration)
+}
