c1c2fbd015549841c66b731082358d79753ce5bc
kseito <torinokonoko@gmail.com>
2025-05-30 09:01:08 +0900

    Remove unused import in RewardListScreen

--- feature/reward/src/main/java/jp/kztproject/rewardedtodo/feature/reward/list/RewardListScreen.kt
+++ feature/reward/src/main/java/jp/kztproject/rewardedtodo/feature/reward/list/RewardListScreen.kt
@@ -3,7 +3,6 @@ package jp.kztproject.rewardedtodo.feature.reward.list
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
