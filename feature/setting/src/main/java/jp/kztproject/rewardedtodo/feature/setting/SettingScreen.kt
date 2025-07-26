d748e5889f6af071232f740de0e55b96c92f24e0
kseito <torinokonoko@gmail.com>
2025-07-27 08:10:53 +0900

    Add SettingScreen

---
+++ feature/setting/src/main/java/jp/kztproject/rewardedtodo/feature/setting/SettingScreen.kt
@@ -0,0 +1,9 @@
+package jp.kztproject.rewardedtodo.feature.setting
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingScreen() {
+    Text("SettingScreen")
+}
