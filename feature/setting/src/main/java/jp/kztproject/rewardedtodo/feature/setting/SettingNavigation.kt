93ac3cf8de0f90331181ced7c269f38ac391dc03
kseito <torinokonoko@gmail.com>
2025-07-27 08:11:07 +0900

    Add SettingNavigation.kt

---
+++ feature/setting/src/main/java/jp/kztproject/rewardedtodo/feature/setting/SettingNavigation.kt
@@ -0,0 +1,12 @@
+package jp.kztproject.rewardedtodo.feature.setting
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+
+const val SETTING_SCREEN = "setting_screen"
+
+fun NavGraphBuilder.settingScreen() {
+    composable(SETTING_SCREEN) {
+        SettingScreen()
+    }
+}
