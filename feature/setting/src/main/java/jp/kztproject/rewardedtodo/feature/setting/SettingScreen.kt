ded1357e33f83de62cc330335340a1d9e787c251
kseito <torinokonoko@gmail.com>
2025-07-27 14:19:29 +0900

    Support edge-to-edge

--- feature/setting/src/main/java/jp/kztproject/rewardedtodo/feature/setting/SettingScreen.kt
+++ feature/setting/src/main/java/jp/kztproject/rewardedtodo/feature/setting/SettingScreen.kt
@@ -3,6 +3,8 @@ package jp.kztproject.rewardedtodo.feature.setting
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Switch
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -48,7 +50,9 @@ private fun SettingScreenContent(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.systemBars)
             .padding(16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
         Text(
             text = stringResource(R.string.settings_title),
