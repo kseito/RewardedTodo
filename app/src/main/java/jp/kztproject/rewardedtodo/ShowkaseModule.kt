9878462677060d9b83406098b1203e0bb2450b6f
Kazuto Seito <kazuto.seito@studist.jp>
2025-08-15 18:17:03 +0900

    Add Showkase module definition

---
+++ app/src/main/java/jp/kztproject/rewardedtodo/ShowkaseModule.kt
@@ -0,0 +1,7 @@
+package jp.kztproject.rewardedtodo
+
+import com.airbnb.android.showkase.annotation.ShowkaseRoot
+import com.airbnb.android.showkase.annotation.ShowkaseRootModule
+
+@ShowkaseRoot
+class ShowkaseModule : ShowkaseRootModule
