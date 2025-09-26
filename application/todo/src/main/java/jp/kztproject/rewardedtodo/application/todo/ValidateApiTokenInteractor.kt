e9debb01cea203a278e9822847c46f2123e0f887
Kazuto Seito <kazuto.seito@studist.jp>
2025-09-26 13:57:48 +0900

    feat: add ValidateApiTokenInteractor and ValidateApiTokenUseCase for API token validation

---
+++ application/todo/src/main/java/jp/kztproject/rewardedtodo/application/todo/ValidateApiTokenInteractor.kt
@@ -0,0 +1,32 @@
+package jp.kztproject.rewardedtodo.application.todo
+
+import jp.kztproject.rewardedtodo.domain.todo.ApiToken
+import jp.kztproject.rewardedtodo.domain.todo.TokenError
+import kotlinx.coroutines.TimeoutCancellationException
+import javax.inject.Inject
+import kotlinx.coroutines.withTimeout
+import java.util.concurrent.TimeoutException
+
+class ValidateApiTokenInteractor @Inject constructor() : ValidateApiTokenUseCase {
+
+    override suspend fun execute(tokenValue: String): Result<Unit> {
+        return try {
+            withTimeout(TIMEOUT_MILLIS) {
+                // Try to create ApiToken - this validates the format
+                ApiToken(tokenValue)
+                Result.success(Unit)
+            }
+        } catch (e: Exception) {
+            when (e) {
+                is TimeoutException,
+                is TimeoutCancellationException ->
+                    Result.failure(TokenError.Timeout)
+                else -> Result.failure(TokenError.NetworkError(e))
+            }
+        }
+    }
+
+    private companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
