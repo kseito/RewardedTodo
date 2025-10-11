e67181644938c443dbc3df66b76d08f4c217bae0
kseito <torinokonoko@gmail.com>
2025-10-11 15:50:58 +0900

    feat: migrate bottom sheets to Material3 components and update state management

--- feature/todo/src/main/java/jp/kztproject/rewardedtodo/presentation/todo/TodoListScreen.kt
+++ feature/todo/src/main/java/jp/kztproject/rewardedtodo/presentation/todo/TodoListScreen.kt
@@ -1,5 +1,3 @@
-@file:OptIn(ExperimentalMaterialApi::class)
-
 package jp.kztproject.rewardedtodo.presentation.todo
 
 import androidx.compose.foundation.Image
@@ -14,18 +12,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
-import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.material3.ripple
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
@@ -33,7 +31,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -60,30 +57,24 @@ import jp.kztproject.rewardedtodo.domain.todo.Todo
 import jp.kztproject.rewardedtodo.feature.todo.R
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.launch
 
-@ExperimentalMaterialApi
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun TodoListScreenWithBottomSheet(viewModel: TodoListViewModel = hiltViewModel()) {
-    val coroutineScope = rememberCoroutineScope()
-    val bottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
+    val sheetState = rememberModalBottomSheetState()
     var selectedTodo: Todo? by remember { mutableStateOf(null) }
+    var showBottomSheet by remember { mutableStateOf(false) }
+
     val onTodoItemClicked: (Todo) -> Unit = {
-        coroutineScope.launch {
-            selectedTodo = it
-            bottomSheetState.show()
-        }
+        selectedTodo = it
+        showBottomSheet = true
     }
     val onTodoUpdateSucceed: () -> Unit = {
-        coroutineScope.launch {
-            bottomSheetState.hide()
-        }
+        showBottomSheet = false
     }
     val onTodoAddClicked: () -> Unit = {
-        coroutineScope.launch {
-            selectedTodo = null
-            bottomSheetState.show()
-        }
+        selectedTodo = null
+        showBottomSheet = true
     }
     val onTodoSaveSelected: (EditingTodo) -> Unit = {
         viewModel.updateTodo(it)
@@ -92,22 +83,26 @@ fun TodoListScreenWithBottomSheet(viewModel: TodoListViewModel = hiltViewModel()
         viewModel.deleteTodo(it)
     }
 
-    TodoDetailBottomSheet(
-        todo = selectedTodo,
-        bottomSheetState = bottomSheetState,
-        onTodoSaveSelected = onTodoSaveSelected,
-        onTodoDeleteSelected = onTodoDeleteSelected,
-    ) {
+    Box {
         TodoListScreen(
             viewModel = viewModel,
             onTodoItemClicked = onTodoItemClicked,
             onTodoAddClicked = onTodoAddClicked,
             onTodoUpdateSucceed = onTodoUpdateSucceed,
         )
+
+        TodoDetailBottomSheet(
+            showBottomSheet = showBottomSheet,
+            onDismissRequest = { showBottomSheet = false },
+            sheetState = sheetState,
+            todo = selectedTodo,
+            onTodoSaveSelected = onTodoSaveSelected,
+            onTodoDeleteSelected = onTodoDeleteSelected,
+        )
     }
 }
 
-@ExperimentalMaterialApi
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 private fun TodoListScreen(
     viewModel: TodoListViewModel,
