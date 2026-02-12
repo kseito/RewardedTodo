10a757aed453641f8152241de1153dfdd04529d2
Kazuto Seito <kazuto.seito@studist.jp>
2026-02-12 12:44:42 +0900

    refactor: simplify Gradle settings by removing unused plugins and configurations

--- test/reward/build.gradle.kts
+++ test/reward/build.gradle.kts
@@ -1,29 +1,8 @@
-
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.rewardedtodo.android.library)
 }
 
 android {
-    compileSdk = libs.versions.androidCompileSdkVersion.get().toInt()
-
-    defaultConfig {
-        minSdk = libs.versions.androidMinSdkVersion.get().toInt()
-        targetSdk = libs.versions.androidTargetSdkVersion.get().toInt()
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    kotlin {
-        jvmToolchain(17)
-    }
     namespace = "jp.kztproject.rewardedtodo.test.reward"
 }
 
