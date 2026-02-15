c74b34f63506f9cd923867861b2eee3da33b2e49
kseito <torinokonoko@gmail.com>
2026-02-15 15:57:51 +0900

    refactor: update Todoist API endpoints and model references for consistency

--- data/todoist/src/main/java/jp/kztproject/rewardedtodo/data/todoist/TodoistApi.kt
+++ data/todoist/src/main/java/jp/kztproject/rewardedtodo/data/todoist/TodoistApi.kt
@@ -1,6 +1,6 @@
 package jp.kztproject.rewardedtodo.data.todoist
 
-import jp.kztproject.rewardedtodo.data.todoist.model.Task
+import jp.kztproject.rewardedtodo.data.todoist.model.Tasks
 import jp.kztproject.rewardedtodo.data.todoist.model.TodoistAuthentication
 import retrofit2.Response
 import retrofit2.http.*
@@ -14,9 +14,9 @@ interface TodoistApi {
         @Field("code") code: String
     ): TodoistAuthentication
 
-    @GET("rest/v2/tasks")
-    suspend fun fetchTasks(@Query("filter") filter: String): List<Task>
+    @GET("api/v1/tasks/filter")
+    suspend fun fetchTasks(@Query("query") filter: String): Tasks
 
-    @POST("rest/v2/tasks/{todoist_id}/close")
-    suspend fun completeTask(@Path("todoist_id") todoistId: Long): Response<Unit>
+    @POST("api/v1/tasks/{todoist_id}/close")
+    suspend fun completeTask(@Path("todoist_id") todoistId: String): Response<Unit>
 }
