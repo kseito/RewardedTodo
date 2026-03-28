b813dca769d03a6e05bdfb49d63bf9b3c7818239
kseito <torinokonoko@gmail.com>
2026-03-29 08:41:31 +0900

    feat: remove TicketRepository from CompleteTodoInteractor and update ticket handling logic

--- application/todo/src/main/java/jp/kztproject/rewardedtodo/application/reward/CompleteTodoInteractor.kt
+++ application/todo/src/main/java/jp/kztproject/rewardedtodo/application/reward/CompleteTodoInteractor.kt
@@ -1,17 +1,14 @@
 package jp.kztproject.rewardedtodo.application.reward
 
-import jp.kztproject.rewardedtodo.data.ticket.ITicketRepository
 import jp.kztproject.rewardedtodo.domain.todo.Todo
 import jp.kztproject.rewardedtodo.domain.todo.repository.ITodoRepository
 import javax.inject.Inject
 
 class CompleteTodoInteractor @Inject constructor(
     private val todoRepository: ITodoRepository,
-    private val ticketRepository: ITicketRepository
 ) : CompleteTodoUseCase {
 
     override suspend fun execute(todo: Todo) {
         todoRepository.complete(todo)
-        ticketRepository.addTicket(todo.numberOfTicketsObtained)
     }
 }
