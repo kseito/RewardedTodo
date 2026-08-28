05b4005cf95df3229b90e0bc52fc2bfe97767d1d
kseito <torinokonoko@gmail.com>
2026-08-28 12:49:06 +0900

    Update UseCaseModuleをOAuth認証のUseCaseに差し替え

--- app/src/main/java/jp/kztproject/rewardedtodo/di/UseCaseModule.kt
+++ app/src/main/java/jp/kztproject/rewardedtodo/di/UseCaseModule.kt
@@ -4,28 +4,41 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import jp.kztproject.rewardedtodo.application.todo.DeleteApiTokenInteractor
-import jp.kztproject.rewardedtodo.application.todo.DeleteApiTokenUseCase
-import jp.kztproject.rewardedtodo.application.todo.GetApiTokenInteractor
-import jp.kztproject.rewardedtodo.application.todo.GetApiTokenUseCase
-import jp.kztproject.rewardedtodo.application.todo.SaveApiTokenInteractor
-import jp.kztproject.rewardedtodo.application.todo.SaveApiTokenUseCase
-import jp.kztproject.rewardedtodo.application.todo.ValidateApiTokenInteractor
-import jp.kztproject.rewardedtodo.application.todo.ValidateApiTokenUseCase
+import jp.kztproject.rewardedtodo.application.todo.CompleteTodoistAuthInteractor
+import jp.kztproject.rewardedtodo.application.todo.CompleteTodoistAuthUseCase
+import jp.kztproject.rewardedtodo.application.todo.DisconnectTodoistInteractor
+import jp.kztproject.rewardedtodo.application.todo.DisconnectTodoistUseCase
+import jp.kztproject.rewardedtodo.application.todo.GetTodoistCredentialInteractor
+import jp.kztproject.rewardedtodo.application.todo.GetTodoistCredentialUseCase
+import jp.kztproject.rewardedtodo.application.todo.GetValidAccessTokenInteractor
+import jp.kztproject.rewardedtodo.application.todo.GetValidAccessTokenUseCase
+import jp.kztproject.rewardedtodo.application.todo.RefreshTodoistTokenInteractor
+import jp.kztproject.rewardedtodo.application.todo.RefreshTodoistTokenUseCase
+import jp.kztproject.rewardedtodo.application.todo.StartTodoistAuthInteractor
+import jp.kztproject.rewardedtodo.application.todo.StartTodoistAuthUseCase
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 interface UseCaseModule {
 
     @Binds
-    fun bindSaveApiTokenUseCase(interactor: SaveApiTokenInteractor): SaveApiTokenUseCase
+    fun bindGetTodoistCredentialUseCase(interactor: GetTodoistCredentialInteractor): GetTodoistCredentialUseCase
 
     @Binds
-    fun bindValidateApiTokenUseCase(interactor: ValidateApiTokenInteractor): ValidateApiTokenUseCase
+    fun bindStartTodoistAuthUseCase(interactor: StartTodoistAuthInteractor): StartTodoistAuthUseCase
 
     @Binds
-    fun bindGetApiTokenUseCase(interactor: GetApiTokenInteractor): GetApiTokenUseCase
+    fun bindCompleteTodoistAuthUseCase(interactor: CompleteTodoistAuthInteractor): CompleteTodoistAuthUseCase
+
+    // リフレッシュの並走でトークンが相互に無効化されないよう、実装側でMutexを持つ単一インスタンスを共有する
+    @Binds
+    @Singleton
+    fun bindRefreshTodoistTokenUseCase(interactor: RefreshTodoistTokenInteractor): RefreshTodoistTokenUseCase
 
     @Binds
-    fun bindDeleteApiTokenUseCase(interactor: DeleteApiTokenInteractor): DeleteApiTokenUseCase
+    fun bindGetValidAccessTokenUseCase(interactor: GetValidAccessTokenInteractor): GetValidAccessTokenUseCase
+
+    @Binds
+    fun bindDisconnectTodoistUseCase(interactor: DisconnectTodoistInteractor): DisconnectTodoistUseCase
 }
