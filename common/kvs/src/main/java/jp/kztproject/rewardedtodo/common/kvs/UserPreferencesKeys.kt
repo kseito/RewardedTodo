16b101b262b7a212367d34c80bfec4036fdf1d0d
kseito <torinokonoko@gmail.com>
2026-08-28 12:40:35 +0900

    Update UserPreferencesKeysにOAuthのトークンと認証セッション用のキーを追加

--- common/kvs/src/main/java/jp/kztproject/rewardedtodo/common/kvs/UserPreferencesKeys.kt
+++ common/kvs/src/main/java/jp/kztproject/rewardedtodo/common/kvs/UserPreferencesKeys.kt
@@ -1,12 +1,19 @@
 package jp.kztproject.rewardedtodo.common.kvs
 
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object UserPreferencesKeys {
 
     val TODOIST_ACCESS_TOKEN = stringPreferencesKey("todoist_access_token")
 
-    val TODOIST_API_TOKEN = stringPreferencesKey("todoist_api_token")
+    val TODOIST_REFRESH_TOKEN = stringPreferencesKey("todoist_refresh_token")
+
+    val TODOIST_TOKEN_EXPIRES_AT = longPreferencesKey("todoist_token_expires_at")
+
+    val TODOIST_AUTH_STATE = stringPreferencesKey("todoist_auth_state")
+
+    val TODOIST_AUTH_CODE_VERIFIER = stringPreferencesKey("todoist_auth_code_verifier")
 
     val REWARD_USER_ID = stringPreferencesKey("reward_user_id")
 }
