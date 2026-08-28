73525d4faea12abea9dc3deac9bfa1f9d79307dd
kseito <torinokonoko@gmail.com>
2026-08-28 12:41:22 +0900

    Update DisconnectTodoistUseCaseの解除範囲を端末側のみに変更

--- application/todo/src/main/java/jp/kztproject/rewardedtodo/application/todo/DisconnectTodoistUseCase.kt
+++ application/todo/src/main/java/jp/kztproject/rewardedtodo/application/todo/DisconnectTodoistUseCase.kt
@@ -3,7 +3,11 @@ package jp.kztproject.rewardedtodo.application.todo
 interface DisconnectTodoistUseCase {
 
     /**
-     * Todoist側でアクセストークンを失効させ、端末に保存したクレデンシャルを削除する。
+     * 端末に保存したクレデンシャルを削除して連携を解除する。
+     *
+     * Todoist側のアクセス許可は取り消されない。失効APIが `client_secret` を要求し、
+     * 秘密鍵を持たない公開クライアントからは呼び出せないため、Todoist側の取り消しは
+     * ユーザーがTodoistの設定画面から行う必要がある。
      */
     suspend fun execute(): Result<Unit>
 }
