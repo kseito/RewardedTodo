e1968a7e353d3902ede1e36c9277dd467c56d6be
kseito <torinokonoko@gmail.com>
2026-08-29 16:40:27 +0900

    Update UserPreferencesKeysから認証セッション用のキーを削除

--- common/kvs/src/main/java/jp/kztproject/rewardedtodo/common/kvs/UserPreferencesKeys.kt
+++ common/kvs/src/main/java/jp/kztproject/rewardedtodo/common/kvs/UserPreferencesKeys.kt
@@ -11,9 +11,5 @@ object UserPreferencesKeys {
 
     val TODOIST_TOKEN_EXPIRES_AT = longPreferencesKey("todoist_token_expires_at")
 
-    val TODOIST_AUTH_STATE = stringPreferencesKey("todoist_auth_state")
-
-    val TODOIST_AUTH_CODE_VERIFIER = stringPreferencesKey("todoist_auth_code_verifier")
-
     val REWARD_USER_ID = stringPreferencesKey("reward_user_id")
 }
